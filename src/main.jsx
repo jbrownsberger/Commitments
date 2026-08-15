fea33723e260fed41a121675859beb2c9fd76740
jbrownsberger <118187976+jbrownsberger@users.noreply.github.com>
2026-08-15 10:30:30 -0400

    theme: apply F4 Harbor Lamp palette

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './styles/login.css';
+import './styles/harbor-lamp.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
